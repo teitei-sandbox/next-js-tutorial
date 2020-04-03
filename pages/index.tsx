eb07fac705d28dd9edacacb2e5a12520e3ef7cfe
teitei-tk <teitei.tk@gmail.com>
2020-04-03 20:01:27 +0900

    typescript
    https://nextjs.org/learn/excel/typescript/page-types
    https://nextjs.org/learn/excel/typescript/finally

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
-const Home = () => <h1>Hello world!</h1>;
+import { NextPage } from "next";
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+  <h1>Hello world! - user agent: {userAgent}</h1>
+);
+
+Home.getInitialProps = async ({ req }): Promise<{ userAgent: string }> => {
+  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+  return { userAgent };
+};
 
 export default Home;
