f3a810b775405aa71017b70e3d7821aae4208069
teitei-tk <teitei.tk@gmail.com>
2020-04-15 19:53:06 +0900

    Create Dynamic Pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import { NextPage } from "next";
+import Link from "next/link";
+
 import Layout from "../components/MyLayout";
 
-const Index: NextPage<{ userAgent: string }> = ({ userAgent }) => {
+const PostLink: NextPage<{ title: string }> = ({ title }) => {
+  return (
+    <li>
+      <Link href={`/post?title=${title}`}>
+        <a>{title}</a>
+      </Link>
+    </li>
+  );
+};
+
+const Index: NextPage<{}> = () => {
   return (
     <Layout>
-      <p>Hello Next.js World! - userAgent: {userAgent}</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Lern Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   );
 };
